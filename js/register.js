cb585c9d40d41d6a2581da3220c9bceb5099372d
nkhhnh <105964980+nkhhnh@users.noreply.github.com>
2025-07-29 18:03:13 +0700

    change url api

--- js/register.js
+++ js/register.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/register', {
+            const response = await fetch('https://apimusicweather.onrender.com/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
