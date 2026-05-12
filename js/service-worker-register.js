42384824fce243c803ed1af7deca2960335b52f0
Lai Tran Ngoc Khanh <khanhlai6420@gmail.com>
2026-05-12 22:05:40 +0700

    fix volume

--- js/service-worker-register.js
+++ js/service-worker-register.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js')
             .then(registration => {
+                // Ép kiểm tra bản cập nhật mới nhất từ Render ngay lập tức
+                registration.update();
        
                 if (registration.active) {
                     
